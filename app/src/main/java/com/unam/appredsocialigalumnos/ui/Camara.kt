6dbd5a87771538b4bfd293453da43759d58877b7
Carmen <carmenmtz97@comunidad.unam.mx>
2022-06-22 12:20:28 -0500

    Clase Martes Android Intermedio.2.1.1

---
+++ app/src/main/java/com/unam/appredsocialigalumnos/ui/Camara.kt
@@ -0,0 +1,217 @@
+package com.unam.appredsocialigalumnos.ui
+
+import android.media.MediaScannerConnection
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Telephony.Mms.Part.FILENAME
+import android.util.DisplayMetrics
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
+import com.unam.appredsocialigalumnos.R
+import com.unam.appredsocialigalumnos.databinding.ActivityCamaraBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+typealias LumaListener=(luma:Double)->Unit
+
+
+class Camara : AppCompatActivity() {
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    lateinit var binding : ActivityCamaraBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityCamaraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        binding.cameraCaptureButton.setOnClickListener {
+            takePhoto()
+        }
+
+        binding.cameraSwitchButton.setOnClickListener {
+            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
+                CameraSelector.LENS_FACING_BACK
+            } else {
+                CameraSelector.LENS_FACING_FRONT
+            }
+            // Se realiza nuevamente la vinculacion
+            bindCameraUseCases()
+        }
+
+        //Solicitar permisos de uso al usuario.
+        if (allPermissionsGranted()) {
+            startCamera()
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    R.string.proporcionar_permisos,
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+
+            // CameraProvider
+            cameraProvider = cameraProviderFuture.get()
+
+            // Cambio entre camaras dependiendo de la disponibilidad
+            lensFacing = when {
+                hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+                hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+                else -> throw IllegalStateException(R.string.no_existe_dispositivo.toString())
+            }
+
+            // Habilitar o deshabilitar boton de cambio de camaras
+            updateCameraSwitchButton()
+
+            // Uses case
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(this))
+    }
+    private fun takePhoto() {
+        // Se crea el archivo de salida
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Crear archivo de salida incluyendo metadatos.
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        //Capturar la imagen actual de la camara
+        imageCapture?.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Error al realizar la captura de la foto: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+
+                    Toast.makeText(baseContext, R.string.img_guardar.toString()+ "${savedUri}", Toast.LENGTH_SHORT).show()
+
+
+                    // Utilizamos [MediaScannerConnection] para escanear los medios de la galeria
+                    val mimeType = MimeTypeMap.getSingleton()
+                        .getMimeTypeFromExtension(savedUri.toFile().extension)
+                    MediaScannerConnection.scanFile(
+                        baseContext,
+                        arrayOf(savedUri.toFile().absolutePath),
+                        arrayOf(mimeType)
+                    ) { _, uri ->
+                        Log.d(TAG, "Imagen capturada fue guardada: $uri")
+                    }
+                }
+            })
+
+    }
+    private fun updateCameraSwitchButton() {
+        val switchCamerasButton = binding.cameraSwitchButton
+        try {
+            switchCamerasButton.isEnabled = hasBackCamera() && hasFrontCamera()
+        } catch (exception: CameraInfoUnavailableException) {
+            switchCamerasButton.isEnabled = false
+        }
+    }
+    /** Devuelte true si el dispositivo cuenta con camara trasera, caso contrario devuelve false */
+    private fun hasBackCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+    }
+
+    /** Devuelte true si el dispositivo cuenta con camara frontal, caso contrario devuelve false */
+    private fun hasFrontCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+    }
+    private fun bindCameraUseCases() {
+
+        // Metricas para determinar tamaño completo de pantalla
+        val metrics = DisplayMetrics().also { binding.viewFinder.display.getRealMetrics(it) }
+        Log.d(TAG, "Pantalla: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        Log.d(TAG, "Aspecto: $screenAspectRatio")
+
+        val rotation = binding.viewFinder.display.rotation
+
+        // CameraProvider
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Error al iniciar la camara.")
+
+        // CameraSelector
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        // Preview
+        preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        // ImageCapture
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        // ImageAnalysis
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+            // Se asigna a una instancia
+            .also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                    Log.d(TAG, "Average luminosity: $luma")
+                })
+            }
+
+        // Desvincular para poder usar de nuevo
+        cameraProvider.unbindAll()
+
+        try {
+            //Vincular la camara a "use cases"
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+            // Generar vista previa de la camara
+            preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case fallo", exc)
+
+}
