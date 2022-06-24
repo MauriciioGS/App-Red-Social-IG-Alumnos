81b01518358fccbe49b85482ab898ad1e513fb3c
Carmen <carmenmtz97@comunidad.unam.mx>
2022-06-24 12:11:34 -0500

    Clase Viernes Android Intermedio

--- app/src/main/java/com/unam/appredsocialigalumnos/ui/LoginFragment.kt
+++ app/src/main/java/com/unam/appredsocialigalumnos/ui/LoginFragment.kt
@@ -1,21 +1,89 @@
 package com.unam.appredsocialigalumnos.ui
 
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.unam.appredsocialigalumnos.R
 import com.unam.appredsocialigalumnos.databinding.FragmentHostLoginBinding
 import com.unam.appredsocialigalumnos.util.findNavControllerSafely
 
+const val USERNAME_KEY = "username_key"
+
 class LoginFragment :  FragmentBase<FragmentHostLoginBinding>(
     R.layout.fragment_host_login, FragmentHostLoginBinding::bind) {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            reload()
+        }
+    }
+
+    private fun reload() {
+
+
+
+    }
 
     override fun initElements() {
         binding.tvSignUp.setOnClickListener {
             findNavControllerSafely()?.navigate(R.id.action_loginFragment_to_signUpFragment)
         }
         binding.btnLogin.setOnClickListener {
-            Intent(requireContext(),NavigationActivity::class.java ).let(::startActivity)
-        requireActivity().finish()}
-
+            val email = binding.usernameTextField.editText?.text.toString()
+            val password = binding.passwordTextField.editText?.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                signIn(email,password)
+            }
+        }
     }
 
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    startNavMainActivity(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        requireContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startNavMainActivity(null)
+                }
+            }
+// [END sign_in_with_email]
+    }
+
+    private fun startNavMainActivity(user: FirebaseUser?){
+        val intent = Intent(requireContext(),NavigationActivity::class.java)
+        intent.putExtra(USERNAME_KEY,user)
+        startActivity(intent)
+        requireActivity().finish()
+
+
+
+    }
+    companion object{
+        private const val TAG="EmailPassword"
+
+    }
 }
--- app/src/main/java/com/unam/appredsocialigalumnos/ui/SignUpFragment.kt
+++ app/src/main/java/com/unam/appredsocialigalumnos/ui/SignUpFragment.kt
@@ -1,17 +1,83 @@
 package com.unam.appredsocialigalumnos.ui
 
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.unam.appredsocialigalumnos.R
 import com.unam.appredsocialigalumnos.databinding.FragmentSingUpBinding
 
 
 class SignUpFragment : FragmentBase<FragmentSingUpBinding>(
     R.layout.fragment_sing_up,FragmentSingUpBinding::bind) {
-    override fun initElements() {
-        binding.btnRegister.setOnClickListener {
 
-                Intent(requireContext(),NavigationActivity::class.java ).let(::startActivity)
-            requireActivity().finish()
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
         }
     }
+
+    private fun reload() {
+
+
+    }
+
+
+    override fun initElements() {
+        binding.btnRegister.setOnClickListener {
+            val username = binding.tvUsernameSignup.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.editPasswordSingup.text.toString()
+            val repassword = binding.editRePasswordSignup.toString()
+            if (username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && repassword.isNotEmpty()) {
+                if (password == repassword) {
+                    createAccount(email, password)
+                }
+            }
+
+        }
+    }
+    private fun createAccount(email:String,password:String){
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    startNavMainActivity(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(requireContext(), "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    startNavMainActivity(null)
+                }
+            }
+        // [END create_user_with_email]
+
+    }
+    private fun startNavMainActivity(user: FirebaseUser?) {
+        val intent = Intent(requireContext(),NavigationActivity::class.java)
+        intent.putExtra(USERNAME_KEY, user)
+        startActivity(intent)
+        requireActivity().finish()
+    }
+    companion object{
+        private const val TAG="EmailPassword"
+    }
 }
+
